2d7cc6c8e2a615bbf62ec59231e27de64577821f
Efrain Palencia <efrainpalencia@hotmail.com>
2025-12-25 23:46:47 -0500

    Installs vite-pwa and adds generic template to config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,58 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+
+      includeAssets: [
+        "icons/icon-192.png",
+        "icons/icon-512.png"
+      ],
+
+      manifest: {
+        name: "Poker Rules",
+        short_name: "Poker Rules",
+        description: "Internal poker rules reference",
+        theme_color: "#0f172a",
+        background_color: "#ffffff",
+        display: "standalone",
+        start_url: "/rb/9-24-24-x7k3p/",
+        scope: "/rb/9-24-24-x7k3p/",
+        icons: [
+          {
+            src: "/icons/icon-192.png",
+            sizes: "192x192",
+            type: "image/png"
+          },
+          {
+            src: "/icons/icon-512.png",
+            sizes: "512x512",
+            type: "image/png"
+          }
+        ]
+      },
+
+      workbox: {
+        runtimeCaching: [
+          {
+            urlPattern: ({ url }) =>
+              url.pathname.startsWith("/rules/"),
+            handler: "CacheFirst",
+            options: {
+              cacheName: "rules-cache",
+              expiration: {
+                maxEntries: 50,
+                maxAgeSeconds: 60 * 60 * 24 * 365 // 1 year
+              }
+            }
+          }
+        ]
+      }
+    }),
+  ],
 })
