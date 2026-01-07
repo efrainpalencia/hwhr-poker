b891ebe1d610d9e8ebaf3e68d8739ae63e3e32ce
Efrain Palencia <efrainpalencia@hotmail.com>
2026-01-07 10:23:37 -0500

    Adds separator line to main page

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -32,7 +32,7 @@ export default function MainPage() {
       </h1>
       <h1 className="text-2xl italic">5th Revised Draft</h1>
       <h2 className="m-3">Covered Casinos</h2>
-      <div className="grid grid-cols-3 align-baseline gap-4 mt-6">
+      <div className="grid grid-cols-3 align-baseline gap-4 my-6">
         <img className="rounded-lg" src={pic1} alt="Hard Rock Tampa" />
         <img className="rounded-lg" src={pic2} alt="Hard Rock Hollywood" />
         <img className="rounded-lg" src={pic3} alt="Seminole Brighton" />
@@ -40,7 +40,8 @@ export default function MainPage() {
         <img className="rounded-lg" src={pic5} alt="Seminole Hollywood" />
         <img className="rounded-lg" src={pic6} alt="Seminole Imokalee" />
       </div>
-      <div className="grid grid-cols-1 gap-3 mt-6">
+      <div className="my-3 h-px w-full bg-slate-700 dark:bg-slate-300" />
+      <div className="grid grid-cols-1 place-items-center gap-3 mt-12">
         <p>This document is inteded for use by casino employees.</p>
         <p>
           Seminole Tribe of Florida – Poker – Intended Recipient: Seminole
